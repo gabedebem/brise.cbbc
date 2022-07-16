5a9d339b85b2755931b53127af28ef493c631de1
Alexandre Abreu <alexandregrandeabreu@gmail.com>
2022-07-16 14:48:25 -0300

    testes lux

--- lux/lux.py
+++ lux/lux.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import time
 import board
 import adafruit_bh1750
@@ -6,7 +7,11 @@ i2c = board.I2C()
 sensor_in = adafruit_bh1750.BH1750(i2c, address=0x23)
 sensor_out = adafruit_bh1750.BH1750(i2c, address=0x5C)
 
-while True:
-    # print("%.2f Lux"%sensor.lux)
-    print(f'{sensor_in.lux=}; {sensor_out.lux=}')
-    time.sleep(1)
+def main():
+    while True:
+        # print("%.2f Lux"%sensor.lux)
+        print(f'{sensor_in.lux=}; {sensor_out.lux=}')
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
