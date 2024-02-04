04b6620e39886f7b5536ef8d89261e0f859cc369
Alexandre Abreu <alexandregrandeabreu@gmail.com>
2024-02-04 16:14:39 -0300

    rm --cache

--- auto-commit.sh
+++ auto-commit.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 cd ~/brise.cbbc
+git rm --cache db/brise.db
+git gc
 git add db/brise.db
 timestamp() {
   date +"at %H:%M:%S on %d/%m/%Y"
 }
-# git commit -am "Regular auto-commit $(timestamp)"
-git commit --amend -m "Regular auto-commit ammend DB $(timestamp)"
-git push --force-with-lease
+# git commit --amend -m "Regular auto-commit ammend DB $(timestamp)"
+# git push --force-with-lease
+git commit -am "Regular auto-commit $(timestamp)"
+git push
