ce25aea44e8da5adc7f4e6f4a0e2dafe257bf4b4
Alexandre Abreu <alexandregrandeabreu@gmail.com>
2024-02-04 17:31:45 -0300

    remove force

--- auto-commit.sh
+++ auto-commit.sh
@@ -2,8 +2,8 @@
 cd ~/brise.cbbc
 git rm --cache db/brise.db
 git rm --cache logs/logs_sistema.log
-python3 git-filter-repo.py --invert-paths --path 'db/brise.db' --force
-python3 git-filter-repo.py --invert-paths --path 'logs/logs_sistema.log' --force
+python3 git-filter-repo.py --invert-paths --path 'db/brise.db'
+python3 git-filter-repo.py --invert-paths --path 'logs/logs_sistema.log'
 git add db/brise.db
 git add logs/logs_sistema.log
 timestamp() {
