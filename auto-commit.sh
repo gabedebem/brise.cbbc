28786ae192c73dd90c39f0e904db7967ddc9f9c3
Alexandre Abreu <alexandregrandeabreu@gmail.com>
2024-02-04 15:36:08 -0300

    commit message

--- auto-commit.sh
+++ auto-commit.sh
@@ -5,5 +5,5 @@ timestamp() {
   date +"at %H:%M:%S on %d/%m/%Y"
 }
 # git commit -am "Regular auto-commit $(timestamp)"
-git commit --amend -m "Regular auto-commit $(timestamp)"
+git commit --amend -m "Regular auto-commit ammend DB $(timestamp)"
 git push --force-with-lease
