85ab66f63d66cf5c467e0bfe1f8eebc558999ffd
alegume <alexandre.abreu@ifsc.edu.br>
2024-02-04 16:47:14 -0300

    db backup

--- auto-commit.sh
+++ auto-commit.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 cd ~/brise.cbbc
+git rm --cache db/brise.db
+git rm --cache logs/logs_sistema.log
+git gc
 git add db/brise.db
+git add logs/logs_sistema.log
 timestamp() {
   date +"at %H:%M:%S on %d/%m/%Y"
 }
-# git commit -am "Regular auto-commit $(timestamp)"
-git commit --amend -m "Regular auto-commit ammend DB $(timestamp)"
-git push --force-with-lease
+# git commit --amend -m "Regular auto-commit ammend DB $(timestamp)"
+# git push --force-with-lease
+git commit -am "Regular auto-commit $(timestamp)"
+git push
