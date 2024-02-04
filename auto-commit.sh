c6f6b616b67b679596beaf5a7be435adbc7b578d
Alexandre Abreu <alexandregrandeabreu@gmail.com>
2024-02-04 17:45:13 -0300

    cleaning

--- auto-commit.sh
+++ auto-commit.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 cd ~/brise.cbbc
 git rm --cache db/brise.db
-# git rm --cache logs/logs_sistema.log
 git gc
-# python3 git-filter-repo.py --invert-paths --path 'db/brise.db'
-# python3 git-filter-repo.py --invert-paths --path 'logs/logs_sistema.log'
 git add db/brise.db
-# git add logs/logs_sistema.log
 timestamp() {
   date +"at %H:%M:%S on %d/%m/%Y"
 }
-# git commit --amend -m "Regular auto-commit ammend DB $(timestamp)"
-# git push --force-with-lease
 git commit -am "Regular auto-commit $(timestamp)"
 git push
+
+# git rm --cache logs/logs_sistema.log
+# git add logs/logs_sistema.log
+
+# git commit --amend -m "Regular auto-commit ammend DB $(timestamp)"
+# git push --force-with-lease
+
+# python3 git-filter-repo.py --invert-paths --path 'db/brise.db'
+# python3 git-filter-repo.py --invert-paths --path 'logs/logs_sistema.log'
