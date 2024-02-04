82f9bff2839a7f5bb3f34e94a7e2aa8aff3a47ca
Alexandre Abreu <alexandregrandeabreu@gmail.com>
2024-02-04 17:42:50 -0300

    comit

--- auto-commit.sh
+++ auto-commit.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 cd ~/brise.cbbc
 git rm --cache db/brise.db
-git rm --cache logs/logs_sistema.log
+# git rm --cache logs/logs_sistema.log
 git gc
 # python3 git-filter-repo.py --invert-paths --path 'db/brise.db'
 # python3 git-filter-repo.py --invert-paths --path 'logs/logs_sistema.log'
 git add db/brise.db
-git add logs/logs_sistema.log
+# git add logs/logs_sistema.log
 timestamp() {
   date +"at %H:%M:%S on %d/%m/%Y"
 }
